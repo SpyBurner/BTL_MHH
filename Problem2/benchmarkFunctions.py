927d66b4ace134f06fc3ad9031c462d11ba02cfc
SpyBurner <zombiepigman1505@gmail.com>
2023-12-05 00:39:45 +0700

    before benchmark fix

--- Problem2/benchmarkFunctions.py
+++ Problem2/benchmarkFunctions.py
@@ -1,14 +1,11 @@
-import decimal
-from decimal import Decimal
 import time
 ###TIME
 # Define a global variable to store the starting time
-decimal.getcontext().prec = 20
-start_time = Decimal(0.0)
+start_time = 0.0
 # Define a function to set the starting time
 def set_start_time():
     global start_time
-    start_time = Decimal(time.time())
+    start_time = time.time()
     # print("Starting time set to", start_time)
 # Define a function to output the elapsed time
 def get_elapsed_time():
@@ -16,6 +13,6 @@ def get_elapsed_time():
     if start_time is None:
         print("Please set the starting time first")
     else:
-        elapsed_time = Decimal(Decimal(time.time()) - start_time)
-        print("Elapsed time is", format(elapsed_time, ".20f"), "seconds")
+        elapsed_time =time.time() - start_time
+        print("Elapsed time is ", elapsed_time, " seconds")
 ###
